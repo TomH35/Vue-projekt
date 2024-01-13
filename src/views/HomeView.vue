61fef5cd5ecdd8aad477bad447f54e018a47349b
TomH35 <132070682+TomH35@users.noreply.github.com>
2024-01-13 18:54:08 +0100

    Added HomeView

---
+++ src/views/HomeView.vue
@@ -0,0 +1,70 @@
+<template>
+     <!--banner s textom-->
+     <section class="custom-banner">
+      <div class="container h-100">
+        <div class="row h-100 align-items-center">
+          <div class="col-12 text-center">
+            <h1 class="custom-title-color"> <strong> Systémy na čipoch</strong></h1>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </section>
+       <section class="container mt-3 mb-5">
+          <h2 class="text-center"> <strong>Systém na čipe</strong> </h2>
+          <div class="row">
+            <div class="col-sm-6 mt-3">
+              <!--carousel-->
+              <div id="carouselControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <!--prvy item carouselu-->
+                  <div class="carousel-item active">
+                    <img src="/img/shiwa-id-tNYVuRJlc2c-unsplash.jpg" class="d-block w-100" alt="Xiaomi phone">
+                  </div>
+                  <!--druhy item carouselu-->
+                  <div class="carousel-item">
+                    <img src="/img/pexels-torsten-dettlaff-3623360.jpg" class="d-block w-100" alt="iphone">
+                  </div>
+                  <!--treti item carouselu-->
+                  <div class="carousel-item">
+                    <img src="/img/pexels-atahan-demir-12563780.jpg" class="d-block w-100" alt="phone">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselControls" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselControls" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+            </div>
+            <!--Text vedla carouselu-->
+            <div class="col-sm-6 mt-3">
+              <p> Jedná sa o integrovaný obvod, ktorého súčasťou sú všetky komponenty počítača. Môže obsahovať digitálne, analógové, zmiešané a často rádiofrekvenčné funkcie na jednom substráte čipu. Systémy na čipe sú kvôli svojej nizkej spotrebe a relatívne vysokému výkonu vzhľadom na spotrebu energie najpouživanejšími čipmi pre mobilné zariadenia. </p>
+              <p><a href="https://sk.wikipedia.org/wiki/Syst%C3%A9m_na_%C4%8Dipe" class="btn custom-button-color">Zdroj</a></p>
+            </div>
+        </div>
+        </section>
+        <hr>
+        <section class="container mb-3">
+          <h2 class="text-center"> <strong>Snapdragon</strong></h2>
+          <div class="row mt-3">
+            <div class="col-sm-6">
+              <p>Snapdragon je súprava systémov na čipoch (SoC), pre mobilné zariadenia, ktoré vytvorila spoločnosť Qualcomm Technologies Inc. 
+                Centrálna procesorová jednotka (CPU) Snapdragon SoC využíva architektúru ARM. 
+                Jedno SoC môže obsahovať viacero rôznych jadier CPU, grafický procesor Adreno (GPU), bezdrôtový modem Snapdragon, digitálny signálový procesor Hexagon (DSP), procesor obrazového signálu Qualcomm Spectra (ISP) a ďalší softvér a hardvér na podporu smartfónu. globálny systém určovania polohy (GPS), fotoaparát, video, zvuk, rozpoznávanie gest a AI akceleráciu. 
+                Produktové rady Snapdragonu sú 400, 600, 700, 800. </p>
+                <p><a href="https://en.wikipedia.org/wiki/Qualcomm_Snapdragon" class="btn custom-button-color">Zdroj</a></p>
+              </div>
+                <div class="col-sm-6">
+                  <!--obrazok s popisom-->
+                  <figure class="figure">
+                    <img src="/img/graf1.png" class="figure-img img-fluid rounded w-100" alt="Graf výkonu Snapdragon SoC">
+                    <figcaption class="figure-caption text-center">Vývoj grafického výkonu Snapdragon SoC</figcaption>
+                  </figure>
+            </div>
+          </div>
+        </section>
+</template>
